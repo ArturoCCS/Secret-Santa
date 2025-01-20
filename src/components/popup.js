95694e4442a3a54f4598ca8b39a12c4c9e89278a
ArturoCCS <arturoduranaguilar06@gmail.com>
2025-01-19 23:13:33 -0600

    update popup.js

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,33 +1,74 @@
-export function showNamePopup() {
+export function showCreateRoomPopup() {
     return new Promise((resolve, reject) => {  
         const modal = document.createElement('div');
         modal.classList.add('modal');
         modal.innerHTML = `
             <div class="modal-content">
-                <h3>Ingresa tu nombre</h3>
-                <input type="text" id="userNameInput" placeholder="Escribe tu nombre">
-                <button id="submitNameBtn">Aceptar</button>
-                <p id="error-message" style="color: red; display: none;">Por favor, ingresa un nombre válido</p> <!-- Mensaje de error -->
+                <h3 class="title_popup">Ingresa tu nombre</h3>
+                <input class="input" type="text" id="userNameInput" placeholder="Escribe tu nombre">
+                <button id="submitCreateRoomBtn">Aceptar</button>
+                <button id="cancelJoinRoomBtn">Cancelar</button>
             </div>
         `;
 
         document.body.appendChild(modal);
 
-        document.getElementById('submitNameBtn').addEventListener('click', () => {
+        document.getElementById('submitCreateRoomBtn').addEventListener('click', () => {
             const userName = document.getElementById('userNameInput').value.trim();
-            const errorMessage = document.getElementById('error-message');
 
             if (userName) {
                 document.body.removeChild(modal);  
                 resolve(userName);  
-            } else {
-                errorMessage.style.display = 'block';  
-                setTimeout(() => {
-                    errorMessage.style.display = 'none';  
-                }, 2000);
-                reject('No se ingresó un nombre válido');  
+            } else {  
+                reject('No se ingresó un nombre');  
                 document.body.removeChild(modal);  
             }
         });
+        document.getElementById('cancelJoinRoomBtn').addEventListener('click', () => {
+            document.body.removeChild(modal);
+            reject('CANCELLED');
+          });
+    });
+}
+
+export function showJoinRoomPopup(){
+    return new Promise((resolve, reject) => {
+        const modal = document.createElement('div');
+        modal.classList.add('modal');
+        modal.innerHTML = `
+             <div class="modal-content">
+                <h3 class="title_popup">Ingresa tu nombre y el codigo de una sala</h3>
+                <input class="input"  type="text" id="name" class="name" placeholder="Escribe tu nombre">
+                <input class="input"  type="text" id="joinRoomInput" placeholder="Código de Sala">
+                <button id="submitJoinRoomBtn">Aceptar</button>
+                <button id="cancelJoinRoomBtn">Cancelar</button>
+            </div>
+        `;
+
+        document.body.appendChild(modal);
+
+        document.getElementById('submitJoinRoomBtn').addEventListener('click', () => {
+            const userName = document.getElementById('name').value.trim();
+            const roomCode = document.getElementById('joinRoomInput').value.trim();
+
+                if (userName ) {
+                    if(roomCode){
+                        document.body.removeChild(modal);  
+                        resolve({userName, roomCode}); 
+                    }else{
+                        reject('No se ingresó un codigo');  
+                        document.body.removeChild(modal);
+                    }
+                } else {   
+                    reject('No se ingresó un nombre');  
+                    document.body.removeChild(modal);
+                }
+
+        });
+
+        document.getElementById('cancelJoinRoomBtn').addEventListener('click', () => {
+            document.body.removeChild(modal);
+            reject('CANCELLED');
+          });
     });
 }
