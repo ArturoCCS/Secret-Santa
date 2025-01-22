b7d4ce5982f356b491c3a608544452ddd10a585b
CCatShark <149800056+ArturoCCS@users.noreply.github.com>
2025-01-21 19:18:50 -0600

    Update roomView.js

--- src/utility/roomView.js
+++ src/utility/roomView.js
@@ -1,34 +1,32 @@
-export function createRoomView(){
+export function createRoomView() {
     const mainRoom = document.createElement('main');
     mainRoom.classList.add('main-content');
     mainRoom.innerHTML = `
-                
-        <nav class="nav-bar"  id="roomNavInfo" >
+        <nav class="nav-bar" id="roomNavInfo">
             <div class="nav-content">
-              <h2>Estás en la Sala: <span id="roomCode"></span></h2>
-              <h2>Bienvenido: <span id="userName"></span></h2>
+              <h2>You are in Room: <span id="roomCode"></span></h2>
+              <h2>Welcome: <span id="userName"></span></h2>
             </div>
         </nav>
 
-           <header class="header-banner" id="roomBanner" >
+        <header class="header-banner" id="roomBanner">
             <div class="banner-content">
-              <h2 class="participants-title">Otros Participantes:</h2>
+              <h2 class="participants-title">Other Participants:</h2>
               <div id="messages" class="participants-list"></div>
             </div>
             <div class="banner-image">
-              <img class="img_writing_santa" src="src/assets/writing_santa.png" alt="Imagen representativa de amigo secreto para sorteo de regalos">
+              <img class="img_writing_santa" src="src/assets/writing_santa.png" alt="Illustration for Secret Santa draw">
             </div>
         </header>
 
-                <div id="roomInfo" >
-            
-            <section class="input-section" >
-                <h2 class="section-title">Preciona para descubrir a su amigos secretos</h2>
+        <div id="roomInfo">
+            <section class="input-section">
+                <h2 class="section-title">Press to reveal your Secret Santa</h2>
     
                 <div class="button-container">
-                    <button class="button-draw" id="play" aria-label="Sortear amigo secreto">
-                        <img src="src/assets//play_circle_outline.png" alt="Ícono para sortear">
-                        Sortear amigo
+                    <button class="button-draw" id="play" aria-label="Start Secret Santa draw">
+                        <img src="src/assets/play_circle_outline.png" alt="Icon for draw">
+                        Reveal Secret Santa
                     </button>
                 </div>
             </section>
@@ -37,4 +35,3 @@ export function createRoomView(){
 
     return mainRoom;
 }
-
