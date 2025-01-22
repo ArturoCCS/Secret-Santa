3f0ae8d0d3039e748e2a1fc5f952d1fab7544a1d
CCatShark <149800056+ArturoCCS@users.noreply.github.com>
2025-01-21 19:22:22 -0600

    Update popup.js

--- src/components/popup.js
+++ src/components/popup.js
@@ -6,10 +6,10 @@ export function showCreateRoomPopup() {
         modal.classList.add('modal');
         modal.innerHTML = `
             <div class="modal-content">
-                <h3 class="title_popup">Ingresa tu nombre</h3>
-                <input class="input" type="text" id="userNameInput" placeholder="Escribe tu nombre">
-                <button id="submitCreateRoomBtn">Aceptar</button>
-                <button id="cancelJoinRoomBtn">Cancelar</button>
+                <h3 class="title_popup">Enter your name</h3>
+                <input class="input" type="text" id="userNameInput" placeholder="Type your name">
+                <button id="submitCreateRoomBtn">Accept</button>
+                <button id="cancelJoinRoomBtn">Cancel</button>
             </div>
         `;
 
@@ -22,28 +22,28 @@ export function showCreateRoomPopup() {
                 document.body.removeChild(modal);  
                 resolve(userName);  
             } else {  
-                reject('No se ingresó un nombre');  
+                reject('No name was entered');  
                 document.body.removeChild(modal);  
             }
         });
         document.getElementById('cancelJoinRoomBtn').addEventListener('click', () => {
             document.body.removeChild(modal);
             reject('CANCELLED');
-          });
+        });
     });
 }
 
-export function showJoinRoomPopup(){
+export function showJoinRoomPopup() {
     return new Promise((resolve, reject) => {
         const modal = document.createElement('div');
         modal.classList.add('modal');
         modal.innerHTML = `
              <div class="modal-content">
-                <h3 class="title_popup">Ingresa tu nombre y el codigo de una sala</h3>
-                <input class="input"  type="text" id="name" class="name" placeholder="Escribe tu nombre">
-                <input class="input"  type="text" id="joinRoomInput" placeholder="Código de Sala">
-                <button id="submitJoinRoomBtn">Aceptar</button>
-                <button id="cancelJoinRoomBtn">Cancelar</button>
+                <h3 class="title_popup">Enter your name and room code</h3>
+                <input class="input" type="text" id="name" class="name" placeholder="Type your name">
+                <input class="input" type="text" id="joinRoomInput" placeholder="Room code">
+                <button id="submitJoinRoomBtn">Accept</button>
+                <button id="cancelJoinRoomBtn">Cancel</button>
             </div>
         `;
 
@@ -53,60 +53,54 @@ export function showJoinRoomPopup(){
             const userName = document.getElementById('name').value.trim();
             const roomCode = document.getElementById('joinRoomInput').value.trim();
 
-                if (userName) {
-                    if(roomCode){
-                        document.body.removeChild(modal);  
-                        resolve({userName, roomCode}); 
-                    }else{
-                        reject('No se ingresó un codigo de sala');  
-                        document.body.removeChild(modal);
-                    }
-                } else {  
-                    if(roomCode){
-                        reject('No se ingresó un nombre');
-                    }else{
-                        reject('No se ingresó ningun dato');  
-                    }
+            if (userName) {
+                if (roomCode) {
+                    document.body.removeChild(modal);  
+                    resolve({ userName, roomCode }); 
+                } else {
+                    reject('No room code was entered');  
                     document.body.removeChild(modal);
                 }
-
+            } else {  
+                if (roomCode) {
+                    reject('No name was entered');
+                } else {
+                    reject('No data was entered');  
+                }
+                document.body.removeChild(modal);
+            }
         });
 
         document.getElementById('cancelJoinRoomBtn').addEventListener('click', () => {
             document.body.removeChild(modal);
             reject('CANCELLED');
-          });
+        });
     });
 }
 
-export function showSecretSantaPopup(secretSanta){
+export function showSecretSantaPopup(secretSanta) {
     const modal = document.createElement('div');
     modal.classList.add('modal');
     modal.innerHTML = `
         <div class="modal-content center">
-            <p class="section-title">Amigo Secreto</p>
-            <img class="secretSantaIcon" src="../assets/santa_snowman_icon.png" alt="Imagen representativa de amigo secreto para sorteo de regalos">
-      
-            <p class="section-secret-santa">Felicidades tu amigo secreto es <span class="subtitle">¡${secretSanta}!</span></p>
-            
-            <button id="close">Cerrar</button>
-               
+            <p class="section-title">Secret Santa</p>
+            <img class="secretSantaIcon" src="../assets/santa_snowman_icon.png" alt="Illustration for Secret Santa draw">
+            <p class="section-secret-santa">Congratulations! Your Secret Santa is <span class="subtitle">¡${secretSanta}!</span></p>
+            <button id="close">Close</button>
         </div>    
     `;
 
     document.body.appendChild(modal);
 
     document.getElementById('close').addEventListener('click', () => {
-      document.body.removeChild(modal);
+        document.body.removeChild(modal);
     });
-
 }
 
-
-export function desativatedPopoup(){
+export function deactivatePopup() {
     const activePopups = document.querySelectorAll('.modal'); 
     activePopups.forEach(popup => {
         popup.remove();
     });
-    cleanSessionSecretSanta()
+    cleanSessionSecretSanta();
 }
